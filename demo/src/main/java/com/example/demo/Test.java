e17305ed75379ac6dea9b67e85b7a3a556f626f2
Mira Abu Rahmeh <mira.a@b-tek.dev>
2025-10-06 15:23:52 +0300

    test test

--- demo/src/main/java/com/example/demo/Test.java
+++ demo/src/main/java/com/example/demo/Test.java
@@ -6,5 +6,6 @@ public class Test {
         System.out.println("test 2");
         System.out.println("test 3");
         System.out.println("test 4");
+        System.out.println("test5");
     }
 }
