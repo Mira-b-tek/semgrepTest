17e6ef45261f764dc191d43b495c49706579914e
Mira Abu Rahmeh <mira.a@b-tek.dev>
2025-10-06 10:42:38 +0300

    add dep

--- demo/src/main/java/com/example/demo/Test.java
+++ demo/src/main/java/com/example/demo/Test.java
@@ -2,6 +2,9 @@ package com.example.demo;
 
 public class Test {
     public  void method(){
-
+        System.out.println("test method");
+        System.out.println("test 2");
+        System.out.println("test 3");
+        System.out.println("test 4");
     }
 }
