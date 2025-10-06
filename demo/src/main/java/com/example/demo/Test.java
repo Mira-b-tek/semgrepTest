c0283231df9d9809f5a4df2e3ce460f24a488687
Mira Abu Rahmeh <mira.a@b-tek.dev>
2025-10-06 10:11:32 +0300

    remove sout

--- demo/src/main/java/com/example/demo/Test.java
+++ demo/src/main/java/com/example/demo/Test.java
@@ -2,9 +2,6 @@ package com.example.demo;
 
 public class Test {
     public  void method(){
-        System.out.println("test method");
-        System.out.println("test 2");
-        System.out.println("test 3");
-        System.out.println("test 4");
+
     }
 }
